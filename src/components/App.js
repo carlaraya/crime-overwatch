c919b1199f388032d7747eccacd3a253e9ab65cc
carlaraya <carl_angelo.araya@upd.edu.ph>
2017-11-05 15:38:16 +0800

    Install semantic, add routing and dummy routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import ReactRouter, { HashRouter, Route, Switch } from 'react-router-dom';
+import Nav from './Nav.js'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>pucha</h1>
-      </div>
+      <HashRouter>
+        <div className='container'>
+          <Nav />
+          <Switch>
+            <Route exact path='/' component={function(){return null;}} />
+            <Route exact path='/battle' component={function(){return null;}} />
+            <Route path='/battle/results' component={function(){return null;}} />
+            <Route path='/popular' component={function(){return null;}} />
+            <Route render={function() {
+              return <p>Not Found</p>
+            }} />
+          </Switch>
+        </div>
+      </HashRouter>
     );
   }
 }
